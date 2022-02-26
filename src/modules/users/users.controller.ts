1aa52cfc9d516df50515274277bdf12df2af45fc
Adrian Szczechura <a.szczechura.98@gmail.com>
2022-02-26 08:41:51 +0100

    feat(frontned): add users delete hnadler

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,31 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller(
+  'users',
+)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+  ) {}
 
   @Post()
-  create(@Body() CreateUserDto: CreateUserDto): void {
-    this.usersService.create(CreateUserDto);
+  create(
+    @Body()
+    CreateUserDto: CreateUserDto,
+  ): void {
+    this.usersService.create(
+      CreateUserDto,
+    );
   }
 
   @Get()
@@ -17,8 +33,27 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param() params): User {
-    return this.usersService.findOneUser(params.id);
+  @Get(
+    ':id',
+  )
+  findOne(
+    @Param()
+    params,
+  ): User {
+    return this.usersService.findOneUser(
+      params.id,
+    );
+  }
+
+  @Delete(
+    ':id',
+  )
+  remove(
+    @Param()
+    params,
+  ) {
+    return this.usersService.deleteUser(
+      params.id,
+    );
   }
 }
